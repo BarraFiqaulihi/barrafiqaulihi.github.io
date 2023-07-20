b660ff12e08f39e5f50ed6c74441bd474a8b0404
BarraFiqaulihi <ibnu.abi.azzam.02@gmail.com>
2023-07-20 16:34:11 +0200

    Add basic count system

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let count = 0;
+
+    function increment() { count++; };
+    function decrement() { count-- };
+    function reset() { count = 0; };
+</script>
+
+<main class="h-screen w-screen container mx-auto overflow-y-auto">
+    <div class="h-full flex flex-col justify-center">
+        <h1 class="text-4xl text-center font-bold">Barra Fiqaulihi</h1>
+        <div class="h-4"></div>
+        <span class="text-2xl text-center font-semibold">Count = {count}</span>
+        <div class="h-4"></div>
+        <div class="flex justify-center">
+            <button class="btn mx-2" on:click={decrement}>-</button>
+            <button class="btn mx-2" on:click={reset}>reset</button>
+            <button class="btn mx-2" on:click={increment}>+</button>
+        </div>
+    </div>
+</main>
